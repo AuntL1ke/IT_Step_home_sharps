980e811d49e41653eff02cdd3de36407873da3f6
AuntL1ke <88139445+AuntL1ke@users.noreply.github.com>
2024-01-22 21:00:18 +0200

    Add files via upload

---
+++ Hothouse.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace ConsoleApp35
+{
+    delegate void HotHouseDeleg();
+
+    internal class HotHouse
+    {
+        private int temperature;
+
+        public int Min { get; set; }
+        public int Max { get; set; }
+
+        public int Temperature
+        {
+            get => temperature;
+            set
+            {
+                temperature = value;
+
+                if (temperature > Max)
+                {
+                    Hot();
+                }
+                else if (temperature < Min)
+                {
+                    Cold();
+                }
+                else
+                {
+                    Well();
+                }
+            }
+        }
+
+        public event HotHouseDeleg TooHot;
+        public event HotHouseDeleg TooCold;
+        public event HotHouseDeleg Fine;
+
+        protected virtual void Hot()
+        {
+            TooHot?.Invoke();
+            Console.WriteLine("Too hot");
+        }
+
+        protected virtual void Cold()
+        {
+            TooCold?.Invoke();
+            Console.WriteLine("Too cold");
+        }
+
+        protected virtual void Well()
+        {
+            Fine?.Invoke();
+            Console.WriteLine("Well");
+        }
+    }
+
+    internal class Heater
+    {
+        public void Warm(HotHouse house)
+        {
+            house.Temperature += 5;
+            Console.WriteLine("Warming up");
+        }
+    }
+    internal class Cooler
+    {
+        public void Cold(HotHouse house)
+        {
+            house.Temperature -= 5;
+            Console.WriteLine("Cooling down");
+        }
+    }
+
+    
+}
--- Program.cs
+++ Program.cs
@@ -4,12 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp24
+namespace ConsoleApp35
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            HotHouse house = new HotHouse()
+            {
+                Min = 10,
+                Max = 20,
+            };
+            Heater heater = new Heater();
+            Cooler cooler = new Cooler();
+
+            Random random = new Random();
+            while(true)
+            {
+                int weather = random.Next(-2, 3);
+                house.Temperature += weather;
+                
+            }
         }
     }
 }
